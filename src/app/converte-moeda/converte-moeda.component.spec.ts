ffa9f896eff37156d19b96b6fb0481e623408fd1
Rafaela <rafaelaffigueiredo1@gmail.com>
2023-02-15 21:34:23 -0300

    teste dos metodos guardar e convertermoeda

--- src/app/converte-moeda/converte-moeda.component.spec.ts
+++ src/app/converte-moeda/converte-moeda.component.spec.ts
@@ -33,9 +33,10 @@ describe(ConverteMoedaComponent.name, () => {
     component = fixture.componentInstance;
     service = TestBed.inject(MoedaService);
     spyOn(service, 'converterMoeda').and.returnValue(of({ 'info': 1, 'result': 1 }))
+    spyOn(component, 'guardar').and.callThrough();
   })
 
-  it(`#${ConverteMoedaComponent.prototype.converter.name} should call "converter" from service when submited`, () => {
+  it(`#${ConverteMoedaComponent.prototype.converter.name} should call "converter" from service when submited and send to local storage`, () => {
     component.moedaOriginal = 'BRL';
     component.moedaDestino = 'USD';
     component.valorEntrada = 1;
@@ -43,7 +44,13 @@ describe(ConverteMoedaComponent.name, () => {
     fixture.detectChanges();
     fixture.debugElement.query(By.css('.btn-submit')).triggerEventHandler('click, null');
     expect(service.converterMoeda).toHaveBeenCalledWith('BRL', 'USD', 1);
+    expect(component.guardar).toHaveBeenCalled();
     expect(component.valorSaida).toEqual(1);
+  })
+
+
+  it(`#${ConverteMoedaComponent.prototype.checar.name} should detect high conversion values`, () => {
+
   })
 
 
